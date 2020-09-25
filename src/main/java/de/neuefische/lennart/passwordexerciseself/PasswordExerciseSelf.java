503bfb36ee0a3bbed5f7a3ded276408faa171bcf
Lennart <lennart.schwier@icloud.com>
2020-09-25 16:39:39 +0200

    Add all features for the day 250920.

--- src/main/java/de/neuefische/lennart/passwordexerciseself/PasswordExerciseSelf.java
+++ src/main/java/de/neuefische/lennart/passwordexerciseself/PasswordExerciseSelf.java
@@ -1,6 +1,42 @@
 package de.neuefische.lennart.passwordexerciseself;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordExerciseSelf {
+
+    public static final String LENGTH = "Error. Password must be between 8 and 12 characters long.";
+    public static final String NO_NUMBER = "Error. Password must contain at least one number.";
+    public static final String NO_LOWER_CASE = "Error. Password must contain at least one lower case character.";
+    public static final String NO_UPPER_CASE = "Error. Password must contain at least one upper case character.";
+    public static final String SPECIAL_CHARACTER = "Error. Only characters a-z and A-Z and 0-9 are allowed.";
+    public static final String CHECK_PASSED = "The password is fine.";
+
+    public static void main(String[] args) {
+
+    }
+
+    // checks a password for the rules
+    public static String checkPassword(String password) {
+        if (!checkPasswordLength(password)) {
+            return LENGTH;
+        }
+        if (!checkContainsNumber(password)) {
+            return NO_NUMBER;
+        }
+        if (!checkLowerCase(password)) {
+            return NO_LOWER_CASE;
+        }
+        if (!checkUpperCase(password)) {
+            return NO_UPPER_CASE;
+        }
+        if (!checkSpecialCharacter(password)) {
+            return SPECIAL_CHARACTER;
+        }
+        return CHECK_PASSED;
+    }
+
+
 
     // checks length of password
     public static boolean checkPasswordLength(String password) {
@@ -20,5 +56,12 @@ public class PasswordExerciseSelf {
     // checks for upper case
     public static boolean checkUpperCase(String password) {
         return password.matches(".*\\p{Upper}.*");
+    }
+
+    // checks for special character
+    public static boolean checkSpecialCharacter(String password) {
+        Pattern patternCheck = Pattern.compile("[a-zA-Z0-9]*");
+        Matcher matcherCheck = patternCheck.matcher(password);
+        return matcherCheck.matches();
     }
 }
--- src/test/java/de/neuefische/lennart/passwordexerciseself/PasswordExerciseSelfTest.java
+++ src/test/java/de/neuefische/lennart/passwordexerciseself/PasswordExerciseSelfTest.java
@@ -1,7 +1,5 @@
 package de.neuefische.lennart.passwordexerciseself;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordExerciseSelfTest {
 
     // checkPasswordLength()
-    @ParameterizedTest(name = "Lenth of password {0} is true or false {1}")
+    @ParameterizedTest(name = "Length of password {0} is true or false {1}")
     @CsvSource({
             "1234567, false",
             "12345678, true",
@@ -18,7 +16,7 @@ class PasswordExerciseSelfTest {
             "1234567890123, false"
 
     })
-    void testLengthcorrect(String input, boolean expectedResult) {
+    void testLengthCorrect(String input, boolean expectedResult) {
         // GIVEN
         // see CsvSource
 
@@ -69,6 +67,7 @@ class PasswordExerciseSelfTest {
         assertEquals(expectedResult, result);
     }
 
+    // checkUpperCase
     @ParameterizedTest(name = "Password {0} contains an upper case letter {1}")
     @CsvSource({
             "D, true",
@@ -83,6 +82,47 @@ class PasswordExerciseSelfTest {
 
         // WHEN
         boolean result = PasswordExerciseSelf.checkUpperCase(input);
+
+        // THEN
+        assertEquals(expectedResult, result);
+    }
+
+    // checkSpecialCharacter
+    @ParameterizedTest(name = "Password {0} contains no special character {1}")
+    @CsvSource({
+            "a, true",
+            "!, false",
+            "!a, false",
+            "a!, false",
+            "a!a, false"
+    })
+    void testContainsNoSpecialCharacter(String input, boolean expectedResult) {
+        // GIVEN
+        // see CsvSource
+
+        // WHEN
+        boolean result = PasswordExerciseSelf.checkSpecialCharacter(input);
+
+        // THEN
+        assertEquals(expectedResult, result);
+    }
+
+    // checkPassword
+    @ParameterizedTest(name = "Password {0} is {1}")
+    @CsvSource({
+            "1234567, Error. Password must be between 8 and 12 characters long.",
+            "abcdefgh, Error. Password must contain at least one number.",
+            "1234567A, Error. Password must contain at least one lower case character.",
+            "1234567a, Error. Password must contain at least one upper case character.",
+            "12345678Aa!, Error. Only characters a-z and A-Z and 0-9 are allowed.",
+            "12345678Aa, The password is fine."
+    })
+    void testPassword(String input, String expectedResult) {
+        // GIVEN
+        // see CsvSource
+
+        // WHEN
+        String result = PasswordExerciseSelf.checkPassword(input);
 
         // THEN
         assertEquals(expectedResult, result);
